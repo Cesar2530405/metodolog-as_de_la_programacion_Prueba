bc6880208c1d71e93d455a309f8f60bb6fa76d8e
Cesar <2530405@upv.edu.mx>
2025-10-27 09:48:46 -0600

    feat: first class

---
+++ src/understanding_variables.py
@@ -0,0 +1,28 @@
+message = "This is my first python variable "
+another_message = "I am really, really, really happy"
+
+# print() - use to show messages in terminal
+# print() - se utiliza para mostrar mensajes en la terminal 
+print(message)
+print(another_message)
+
+"""
+los nombres de variales en python deben nombrarse solo con:
+
+- letras, numeros y guion bajo (espacios)
+- deben comenzar con una letra o con guion bajo, pero NO con numero:
+    ejemplo correcto: message_1 (:) )
+    ejemplo incorrecto: 1_message (x)
+- no utilizar espacios para separar palabras en los nombres en las variables
+- no utilizar palabras reservadas de Python para nombrar variables o archivos
+- deben ser cortos, pero descriptivos 
+- deben ser en ingles 
+- nombres de las variables en minusculas
+
+
+
+"""
+
+
+charly_message = "Hola soy Charly y estoy aprendiendo Python"
+print (charly_message)
