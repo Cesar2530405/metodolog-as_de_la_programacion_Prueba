d24228c98f3aacdf4dbe51147478385bc1bb28cf
Cesar <2530405@upv.edu.mx>
2025-10-31 10:34:01 -0600

    feat: understanding numbers

---
+++ src/understanding_numbers.py
@@ -0,0 +1,117 @@
+# Numbers
+
+
+"""
+Integers - Enteros 
+
+Son numeros sin punto decimal,
+-infty , ..., -2, -1, 0, 1, 2, ... hasta infinito
+
+Ejemplo:
+
+
+#tipado dinamico 
+age=33
+
+las podemos sumar (+), restar (-), multiplicar (*) y dividir (/)
+
+Potencias (**2, **3,)
+
+Modulo (Dividiendo%Divisor)
+
+
+"""
+
+number_1 = 39
+number_2 = 13
+
+suma = number_1+number_2
+difference = number_1-number_2
+multiplication = number_1*number_2
+division = number_1/number_2
+modulo = number_1%number_2
+power = number_1**2
+
+print("suma: ",suma)
+print("difference: ", difference)
+print("multiplication: ", multiplication)
+print("division:", division)
+print("modulo:", modulo)
+print("power:", power)
+
+print("La suma es del tipo", type(suma))
+print("La resta es del tipo", type(difference))
+print("La multiplicacion es del tipo", type(multiplication))
+print("La division es del tipo", type(division))
+print("La modulo es del tipo", type(modulo))
+print("La power es del tipo", type(power))     
+
+
+
+
+# Floats 
+
+"""
+Floats - Reales
+
+Son numeros con punto decimal, van desde -infty infty
+
+Ejemplo:
+
+
+# Tipado dinamico
+age = 22,5
+
+Las podemos sumar (+), restar (-), multiplicar(*) y dividir (/)
+
+
+"""
+
+print(0.1,+0,2)
+# Numbers
+
+
+"""
+Integers - Enteros 
+
+Son numeros sin punto decimal,
+-infty , ..., -2, -1, 0, 1, 2, ... hasta infinito
+
+Ejemplo:
+
+
+#tipado dinamico 
+age=33
+
+las podemos sumar (+), restar (-), multiplicar (*) y dividir (/)
+
+Potencias (**2, **3,)
+
+Modulo (Dividiendo%Divisor)
+
+
+"""
+
+number_1 = 39
+number_2 = 13
+
+suma = number_1+number_2
+difference = number_1-number_2
+multiplication = number_1*number_2
+division = number_1/number_2
+modulo = number_1%number_2
+power = number_1**2
+
+print("suma: ",suma)
+print("difference: ", difference)
+print("multiplication: ", multiplication)
+print("division:", division)
+print("modulo:", modulo)
+print("power:", power)
+
+print("La suma es del tipo", type(suma))
+print("La resta es del tipo", type(difference))
+print("La multiplicacion es del tipo", type(multiplication))
+print("La division es del tipo", type(division))
+print("La modulo es del tipo", type(modulo))
+print("La power es del tipo", type(power))     
