f4cd5ce1e17d0ed64f5df031a6f1672e46c4abf1
Cesar <2530405@upv.edu.mx>
2025-11-12 12:35:08 -0700

    feat: listas de comprension

---
+++ src/understanding_list_2.py
@@ -0,0 +1,13 @@
+# SLICING
+players = ["cr7", "messi", "Travis", "chicha", "corona"]
+print(players[0:3])
+# Slice es trabajar con un grupo especifico
+# de elementos de una lista
+print(players[1:4]) # ["messi", "Travis", "chicha"]
+print(players[:4]) # players = ["cr7", "messi", "Travis", "chicha"]
+print(players[2:]) 
+
+print(players[-3:])
+
+print(players[-3:-1])
+print(players[4:2]) # No se puede, me va a dar una lista vacia
